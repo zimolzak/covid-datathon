1710befe7f619502fd54d90bd6a8cd508ca80711
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-06-08 16:11:59 -0500

    Narrow lines

--- analysis.R
+++ analysis.R
@@ -1,20 +1,27 @@
 library(ggplot2)
 library(dplyr)
 
-X = read.csv("/Users/ajz/Desktop/covid-2/Random_COVID_PAT.tsv", sep="\t", stringsAsFactors = FALSE)
+#                                                                            #
+
+pathname = "/Users/ajz/Desktop/covid-2/Random_COVID_PAT.tsv"
+
+X = read.csv(pathname, sep="\t", stringsAsFactors = FALSE)
 cov_tests_only = X[X$LAB == "SARS-COV-2", ]
 table(cov_tests_only$ORD_VALUE)
 
 D = 
 X %>% 
-  filter(LAB == "SARS-COV-2") %>%
+filter(LAB == "SARS-COV-2") %>%
 #  slice(1:200) %>%
-  select(PAT_ID, PAT_DOB, SEX_C_ID, ORD_VALUE, IDENTITY_TYPE_ID, PRBL_ICD10, ENC_ICD10, ENC_TYPE) %>%
-  mutate(result = case_when(
-  	ORD_VALUE == "NEGATIVE" | ORD_VALUE == "Negative" | ORD_VALUE == "NOT DETECTED" | ORD_VALUE == "Not Detected" ~ 0,
-  	ORD_VALUE == "POSITIVE" | ORD_VALUE == "Positive" | ORD_VALUE == "DETECTED"     | ORD_VALUE == "Detected" ~ 1,
-  	ORD_VALUE == "PRESUMPTIVE POSITIVE" ~ 0.5
-  	)) 
+select(PAT_ID, PAT_DOB, SEX_C_ID, ORD_VALUE,
+		IDENTITY_TYPE_ID, PRBL_ICD10, ENC_ICD10, ENC_TYPE) %>%
+mutate(result = case_when(
+	ORD_VALUE == "NEGATIVE" | ORD_VALUE == "Negative" | 
+		ORD_VALUE == "NOT DETECTED" | ORD_VALUE == "Not Detected" ~ 0,
+	ORD_VALUE == "POSITIVE" | ORD_VALUE == "Positive" |
+		ORD_VALUE == "DETECTED" | ORD_VALUE == "Detected" ~ 1,
+	ORD_VALUE == "PRESUMPTIVE POSITIVE" ~ 0.5
+	))
 
 D %>%
   group_by(ENC_TYPE) %>%
