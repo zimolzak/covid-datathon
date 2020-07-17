106c2b31f1c3b1ea6743bf19eadc1abed954ce4a
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-07-17 10:39:20 -0500

    Tune up old analysis file

--- analysis.R
+++ analysis.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 library(dplyr)
-#                                                                            #
+#                                                                         80 #
 
-pathname = "/Users/ajz/Desktop/covid-2/Random_COVID_PAT.tsv"
+pathname = "/Users/ajz/Documents/local-git/covid_datathon/Random_COVID_PAT.tsv"
 X = read.csv(pathname, sep="\t", stringsAsFactors = FALSE)
 
 D = 
@@ -46,11 +46,19 @@ D %>%
 #    filter(PAT_ID == "Z1111111") %>% # TODO - fake number
 #    filter(IDENTITY_TYPE_ID == 0)
 
-# one_patient
 
-#### Whole new data layout
+
+
+#### Whole new data layout ########
 
 path = "/Users/ajz/Desktop/covid-2/"
 
 ## define strings for filenames
 ## load in txt/TSV files
+
+
+# PATIENT.txt               # id, mrn, name, dob, age
+# PAT_PRBL_LIST.txt         # id, prob number, date, dx name, icd10
+# PAT_ENC_DX.txt            # id, date, encType, bp, dx_icd
+# Pat_FlowSheet_PulseOx.txt # id, value, time, name (survey question or SpO2)
+# PAT_ORDERS_PROCEDURES.txt # id, date, proc name, result date, ORDER_COMPONENT, ORD_VALUE_TEXT, ord val numeric
