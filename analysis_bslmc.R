2610e26f4731014b5447549ee97bd8d715c1efd9
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-07-31 15:35:17 -0500

    Better organization of comments.

--- analysis_bslmc.R
+++ analysis_bslmc.R
@@ -8,43 +8,45 @@ stluke_lab = read.csv(paste(PATH, FILE,   sep=''), sep="\t", stringsAsFactors = 
 
 ## NOTED_DATE is first date a CC was noted by the provider
 ## NOTED DATE is specific to Problem List!!
-## covid 1 slh is somewhat joined up.
+## covid_1_slh is somewhat joined up.
 ## about 50 pts per TAB file
 
 stluke_lab %>%
 filter(PAT_CLASS == "Outpatient") ->
 out_only
-head(out_only)
-
-
-# (1) Understand Rory's process; Plus Explore the .tab files & understand.
-# Analyses according to thoughts.pdf. What's feasible.
-# Very good that Rory mapped adt_pat_class_c to pat_class and also kept both.
-
-## took lab for covid
-## find all pts who had those lab
-## randomice
-## took first 150 pat ids
-## ran those pat ids over whole hosp record
-
-table(stluke_lab$ADT_PAT_CLASS_C)
-
-#   101    102    103    104    106    129 
-#422644  12248  54964  23553   5910   5436 
-
-table(stluke_lab$PAT_CLASS)
-
-# never been done - TODO -->
-# 	6.	inpatient: testing late in admission: rate of this, rate of positives.  # cool to have ER data
-
-# what is "outpatient"
+# What is "outpatient":
 # still is really outpatient, outpatient test xray us etc. monitoring etc.
 # endoscopy, outpat cath, outpat ed.
+
+table(stluke_lab$ADT_PAT_CLASS_C)
+table(stluke_lab$PAT_CLASS)
+#   101    102    103    104    106    129 
+#422644  12248  54964  23553   5910   5436 
+# Very good that Rory mapped adt_pat_class_c to pat_class and also kept both.
+
+
+######## NOTES
+
+# never been done - TODO -->
+# 	6.	inpatient: testing late in admission: rate of this, rate of positives.
+# cool to have ER data.
+
+# My process:
+# (1) Understand Rory's process; Plus Explore the .tab files & understand.
+# (2) Analyses according to thoughts.pdf. Do what's feasible.
+
+## Data Pull Process is as follows:
+## took lab for covid
+## find all pts who had those lab
+## randomize
+## took first 150 pat ids
+## ran those pat ids over whole hosp record
 
 # Next pull:
 # hosp admission disch dx
 # pulse ox
 # maaaaybe the order date vs result date
-# totally okay to NOT join admissions with the problem list
+# totally okay to NOT join admissions with the problem list.
 
-# where will datathon people get their data dictionaries (Gloria has it: need just a BCM ECA to log in)
+# Where will datathon people get their data dictionaries? (Gloria has it: need just a BCM ECA to log in/)
+# Are people going to want to download and do locally: deident???
