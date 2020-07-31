673f35125729871359eb832379383f0b7e474e18
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-07-31 08:42:07 -0500

    Review proc names.

--- validate_covid_procs.R
+++ validate_covid_procs.R
@@ -4,4 +4,17 @@ setwd(path)
 f_procs = "covid_procs_stlukes.csv"
 procs = read.csv(paste(path, f_procs, sep=''), stringsAsFactors = FALSE)
 
+# TODO - probably don't need the Perl. Can just tell R what NULL means.
+# But it does help me hit spacebar in macOS to skim.
+
 dim(procs) # 29 x 148 (!)
+
+procs %>%
+select(PROC_ID, PROC_NAME, PROC_CODE, IS_ACTIVE_YN, PANEL_C, IS_EC_INACTIVE_YN,
+	IS_PROC_USED, SHORT_NAME, ORDER_DISPLAY_NAME, TEST_ID, BILL_DESC) ->
+selected
+
+	# DEBIT OR CREDIT and TYPE_C have data but seem non useful.
+selected %>%
+select(PROC_ID, PROC_NAME, SHORT_NAME, ORDER_DISPLAY_NAME, BILL_DESC) ->
+names_only
