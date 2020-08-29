5c2fadde7c79fccd23359b70965998c317d1b00d
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-08-29 10:47:30 -0500

    Read in all datasets.

--- bslmc_v4_DataSets_pipe.R
+++ bslmc_v4_DataSets_pipe.R
@@ -3,35 +3,38 @@ library(ggplot2)
 
 PATH = "/Users/ajz/Desktop/covid_datathon_git/DataSets/" # end with slash
 
-PRF = "PROBLEM/PROBLEM_GRP_1 20200814 1853.csv"
-PTF = "PAT_ID_2/PAT_ID_GRP_1 20200814 1359.csv"
-ORF = "ORDER_RESULTS/ORD_RSLTS_GRP_1 20200814 1805.csv"
-HSF = "HSP/HSP_GRP_1 20200814 2205.csv"
-ENF = "ENC_DX/ENC_DX_GRP_1 20200814 1604.csv"
-
-
-#c('PROBLEM/PROBLEM_GRP_3 20200814 1855.csv','PROBLEM/PROBLEM_GRP_2 20200814 1854.csv','PROBLEM/PROBLEM_GRP_1 20200814 1853.csv')
-#c('HSP/HSP_GRP_2 20200814 2207.csv','HSP/HSP_GRP_1 20200814 2205.csv','HSP/HSP_GRP_3 20200814 2208.csv')
-#c('ENC_DX/ENC_DX_GRP_2 20200814 1710.csv','ENC_DX/ENC_DX_GRP_1 20200814 1604.csv','ENC_DX/ENC_DX_GRP_3 20200814 1713.csv')
-#c('PAT_ID_2/PAT_ID_GRP_2 20200814 1407.csv','PAT_ID_2/PAT_ID_GRP_1 20200814 1359.csv','PAT_ID_2/PAT_ID_GRP_3 20200814 1506.csv')
-#c('ORDER_RESULTS/ORDER_RESULTS_GRP_3 20200814 1840.csv','ORDER_RESULTS/ORD_RSLTS_GRP_1 20200814 1805.csv','ORDER_RESULTS/ORDER_RESULTS_GRP_2.csv')
+prl = c('PROBLEM/PROBLEM_GRP_3 20200814 1855.csv','PROBLEM/PROBLEM_GRP_2 20200814 1854.csv','PROBLEM/PROBLEM_GRP_1 20200814 1853.csv')
+hsl = c('HSP/HSP_GRP_2 20200814 2207.csv','HSP/HSP_GRP_1 20200814 2205.csv','HSP/HSP_GRP_3 20200814 2208.csv')
+enl = c('ENC_DX/ENC_DX_GRP_2 20200814 1710.csv','ENC_DX/ENC_DX_GRP_1 20200814 1604.csv','ENC_DX/ENC_DX_GRP_3 20200814 1713.csv')
+ptl = c('PAT_ID_2/PAT_ID_GRP_2 20200814 1407.csv','PAT_ID_2/PAT_ID_GRP_1 20200814 1359.csv','PAT_ID_2/PAT_ID_GRP_3 20200814 1506.csv')
+orl = c('ORDER_RESULTS/ORDER_RESULTS_GRP_3 20200814 1840.csv','ORDER_RESULTS/ORD_RSLTS_GRP_1 20200814 1805.csv','ORDER_RESULTS/ORDER_RESULTS_GRP_2.csv')
 
 # onedrive --> files / covid-19 / covid_datathon / data / DataSets / DATA_HSP_LAB_PROC
 
+list2df <- function(L) {
+	# todo - check about length & class of L
+	d = read.csv(paste(PATH, L[1], sep=''), sep="|", stringsAsFactors = FALSE, na.strings="null")
+	for (i in seq(2,length(L))) {
+		di = read.csv(paste(PATH, L[i], sep=''), sep="|", stringsAsFactors = FALSE, na.strings="null")
+		d %>% bind_rows(di) -> d
+	}
+	return(d)
+}
+
 setwd(PATH)
 
-prob = read.csv(paste(PATH, PRF, sep=''), sep="|", stringsAsFactors = FALSE)
-pat  = read.csv(paste(PATH, PTF, sep=''), sep="|", stringsAsFactors = FALSE)
-ord  = read.csv(paste(PATH, ORF, sep=''), sep="|", stringsAsFactors = FALSE)
-hosp = read.csv(paste(PATH, HSF, sep=''), sep="|", stringsAsFactors = FALSE)
-enc  = read.csv(paste(PATH, ENF, sep=''), sep="|", stringsAsFactors = FALSE)
+prob = list2df(prl)
+pat  = list2df(ptl)
+ord  = list2df(orl)
+hosp = list2df(hsl)
+enc  = list2df(enl)
 
 cat('\nDims of prob, pat, ord, hosp, enc----\n')
-dim(prob) # 132   7
-dim(pat)  # 49  4
-dim(ord)  # 44959     13
-dim(hosp) # 505   33
-dim(enc)  # 222   9
+dim(prob)
+dim(pat)
+dim(ord)  # 181907     13
+dim(hosp)
+dim(enc)
 
 prob %>% #copy paste works
 group_by(PAT_ID) %>%
