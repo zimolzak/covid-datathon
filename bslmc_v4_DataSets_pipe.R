e3256f7c7c8866611b4adb562db0d163dbf36130
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-09-09 08:55:16 -0500

    Format SOME long lines

--- bslmc_v4_DataSets_pipe.R
+++ bslmc_v4_DataSets_pipe.R
@@ -3,45 +3,59 @@ library(ggplot2)
 library(lubridate)
 
 PATH = "/Users/ajz/Desktop/aa-git/covid_datathon/DataSets/" # end with slash
-SETWDPATH = "/Users/ajz/Desktop/aa-git/covid_datathon/" # end with slash
+SETWDPATH = "/Users/ajz/Desktop/aa-git/covid_datathon/"
 
-prl = c('PROBLEM/PROBLEM_GRP_3 20200814 1855.csv','PROBLEM/PROBLEM_GRP_2 20200814 1854.csv','PROBLEM/PROBLEM_GRP_1 20200814 1853.csv')
-hsl = c('HSP/HSP_GRP_2 20200814 2207.csv','HSP/HSP_GRP_1 20200814 2205.csv','HSP/HSP_GRP_3 20200814 2208.csv')
-enl = c('ENC_DX/ENC_DX_GRP_2 20200814 1710.csv','ENC_DX/ENC_DX_GRP_1 20200814 1604.csv','ENC_DX/ENC_DX_GRP_3 20200814 1713.csv')
-ptl = c('PAT_ID_2/PAT_ID_GRP_2 20200814 1407.csv','PAT_ID_2/PAT_ID_GRP_1 20200814 1359.csv','PAT_ID_2/PAT_ID_GRP_3 20200814 1506.csv')
-orl = c('ORDER_RESULTS/ORDER_RESULTS_GRP_3 20200814 1840.csv','ORDER_RESULTS/ORD_RSLTS_GRP_1 20200814 1805.csv','ORDER_RESULTS/ORDER_RESULTS_GRP_2.csv')
+setwd(SETWDPATH)
+
+prl = c('PROBLEM/PROBLEM_GRP_3 20200814 1855.csv',
+    'PROBLEM/PROBLEM_GRP_2 20200814 1854.csv',
+    'PROBLEM/PROBLEM_GRP_1 20200814 1853.csv')
+ptl = c('PAT_ID_2/PAT_ID_GRP_2 20200814 1407.csv',
+    'PAT_ID_2/PAT_ID_GRP_1 20200814 1359.csv',
+    'PAT_ID_2/PAT_ID_GRP_3 20200814 1506.csv')
+orl = c('ORDER_RESULTS/ORDER_RESULTS_GRP_3 20200814 1840.csv',
+    'ORDER_RESULTS/ORD_RSLTS_GRP_1 20200814 1805.csv',
+    'ORDER_RESULTS/ORDER_RESULTS_GRP_2.csv')
+hsl = c('HSP/HSP_GRP_2 20200814 2207.csv',
+    'HSP/HSP_GRP_1 20200814 2205.csv',
+    'HSP/HSP_GRP_3 20200814 2208.csv')
+enl = c('ENC_DX/ENC_DX_GRP_2 20200814 1710.csv',
+    'ENC_DX/ENC_DX_GRP_1 20200814 1604.csv',
+    'ENC_DX/ENC_DX_GRP_3 20200814 1713.csv')
 
 # onedrive --> files / covid-19 / covid_datathon / data / DataSets / DATA_HSP_LAB_PROC
 
 list2df <- function(L) {
-	# todo - check about length & class of L
-	d = read.csv(paste(PATH, L[1], sep=''), sep="|", stringsAsFactors = FALSE, na.strings="null")
-	for (i in seq(2,length(L))) {
-		di = read.csv(paste(PATH, L[i], sep=''), sep="|", stringsAsFactors = FALSE, na.strings="null")
-		d %>% bind_rows(di) -> d
-	}
-	return(d)
+    # todo - check about length & class of L
+    d = read.csv(paste(PATH, L[1], sep=''), sep="|",
+        stringsAsFactors = FALSE, na.strings="null")
+    for (i in seq(2,length(L))) {
+        di = read.csv(paste(PATH, L[i], sep=''), sep="|",
+	    stringsAsFactors = FALSE, na.strings="null")
+        d %>% bind_rows(di) -> d
+    }
+    return(d)
 }
 
-setwd(SETWDPATH)
-
 prob = list2df(prl) %>%
-	select(-PROBLEM_LIST_ID, -DX_ID, -CHRONIC_YN) %>%
-	mutate_at(vars(NOTED_DATE), ~ as.Date( . , '%m/%d/%Y' ))
-pat  = list2df(ptl) %>%
-	select(-IDENTITY_ID) %>%
-	mutate_at(vars(BIRTH_DATE, DEATH_DATE), ~ as.Date( . , '%m/%d/%Y' )) %>%
-	mutate(age = as.numeric(as.Date('2020-08-30', '%Y-%m-%d') - BIRTH_DATE) / 365)
-ord  = list2df(orl) %>%
-	select(-PROC_ID, -LAB_STATUS_C, -COMPONENT_ID) %>%
-	mutate_at(vars(ORDERING_DATE, RESULT_DATE), ~ as.Date( . , '%m/%d/%Y' ))
+    select(-PROBLEM_LIST_ID, -DX_ID, -CHRONIC_YN) %>%
+    mutate_at(vars(NOTED_DATE), ~ as.Date( . , '%m/%d/%Y' ))
+pat = list2df(ptl) %>%
+    select(-IDENTITY_ID) %>%
+    mutate_at(vars(BIRTH_DATE, DEATH_DATE), ~ as.Date( . , '%m/%d/%Y' )) %>%
+    mutate(age = as.numeric(as.Date('2020-08-30', '%Y-%m-%d') - BIRTH_DATE) / 365)
+ord = list2df(orl) %>%
+    select(-PROC_ID, -LAB_STATUS_C, -COMPONENT_ID) %>%
+    mutate_at(vars(ORDERING_DATE, RESULT_DATE), ~ as.Date( . , '%m/%d/%Y' ))
 hosp = list2df(hsl) %>%
-	select(-IDENTITY_ID, -IDENTITY_TYPE_ID, -ID_TYPE_NAME, -HSP_ACCOUNT_ID, -BIRTH_DATE, -ETHNIC_GROUP_C, -DEATH_DATE, -SEX_C, -ADT_PAT_CLASS_C, -LEVEL_OF_CARE_C, -ADMIT_SOURCE_C, -DISCH_DISP_C, -PATIENT_RACE_C) %>%
-	mutate_at(vars(HOSP_ADMSN_TIME, HOSP_DISCH_TIME), ~  parse_date_time( . , orders='mdYIMSp')) %>%
-	mutate(los = difftime(HOSP_DISCH_TIME, HOSP_ADMSN_TIME, units="days"))
-enc  = list2df(enl) %>% select(-ENC_TYPE_C, -enc_dx_id)
-
-
+    select(-IDENTITY_ID, -IDENTITY_TYPE_ID, -ID_TYPE_NAME,
+        -HSP_ACCOUNT_ID, -BIRTH_DATE, -ETHNIC_GROUP_C, -DEATH_DATE,
+        -SEX_C, -ADT_PAT_CLASS_C, -LEVEL_OF_CARE_C, -ADMIT_SOURCE_C,
+        -DISCH_DISP_C, -PATIENT_RACE_C) %>%
+    mutate_at(vars(HOSP_ADMSN_TIME, HOSP_DISCH_TIME), ~  parse_date_time( . , orders='mdYIMSp')) %>%
+    mutate(los = difftime(HOSP_DISCH_TIME, HOSP_ADMSN_TIME, units="days"))
+enc = list2df(enl) %>%
+    select(-ENC_TYPE_C, -enc_dx_id)
 
 cat('\nDims of prob, pat, ord, hosp, enc----\n')
 dim(prob)
@@ -49,6 +63,8 @@ dim(pat)
 dim(ord)  # 181907     13
 dim(hosp) # 2226 x 33
 dim(enc)  # 2068 x 9
+
+#### Hosp
 
 table(hosp$ETHNIC_GROUP)
 table(hosp$PAT_RACE)
@@ -61,6 +77,8 @@ table(hosp$ID_TYPE_NAME)
 table(hosp$NAME)
 table(hosp$PAT_CLASS, hosp$DISCHARGE_DISP)
 
+#### Problem list
+
 prob %>% #copy paste works
 group_by(PAT_ID) %>%
 summarise(dm     = sum(grepl("diab", DX_NAME, ignore.case = TRUE)),
@@ -72,6 +90,8 @@ comorb_count_p
 # pat has birth date and death date. Death date is a character.
 # hosp - no diagnoses?
 
+#### Encounter
+
 enc %>%
 group_by(PAT_ID) %>% ## REUSE
 summarise(dm     = sum(grepl("diab", enc_dx_name, ignore.case = TRUE)),
@@ -79,6 +99,11 @@ summarise(dm     = sum(grepl("diab", enc_dx_name, ignore.case = TRUE)),
 	      copd   = sum(grepl("copd", enc_dx_name, ignore.case = TRUE)),
 	      htn    = sum(grepl("hypert", enc_dx_name, ignore.case = TRUE))) ->
 comorb_count_e
+
+
+
+
+#### Joins
 
 comorb_count_p %>%
 full_join(comorb_count_e, by = "PAT_ID") ->
