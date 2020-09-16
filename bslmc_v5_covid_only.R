7979135b9d541f15f48057068237a9ba79d2a511
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2020-09-16 09:25:47 -0500

    Prep for new data

---
+++ bslmc_v5_covid_only.R
@@ -0,0 +1,7 @@
+# onedrive --> files / covid-19 / covid_datathon / HospitalAdmissionCovid
+
+SETWDPATH = "/Users/ajz/Desktop/aa-git/covid_datathon/"
+DATADIR = "HospitalAdmissionCovid/"
+DATAPATH = paste(SETWDPATH, DATADIR, sep='')
+
+setwd(SETWDPATH)
