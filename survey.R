6c8e66ed7ebef69cf40ee3f30ef4757d229b91bd
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-08-30 15:40:02 -0500

    Rename the very long column names.

--- survey.R
+++ survey.R
@@ -28,19 +28,63 @@ survey_in = filename2df('BCMDatathonSurvey_DATA_2021-08-10.csv')
 say('Dimensions of survey_in')
 dim(survey_in)
 
-survey_in
 
 
 
 
-#### Cleaning (of dates, etc)
+#### Cleaning
+
+survey_in %>%
+rename(
+id = Record.ID,
+sid = Survey.Identifier,
+time = Survey.Timestamp,
+acadRank = Academic.rank,
+acadRank.othertext = Other..specify,
+dept = Primary.department.section.research.center,
+years = Number.of.years.at.BCM,
+prior.hack = I.participated.previously.in.a.hackathon.datathon,
+prior.emrdata = I.had.experience.using.electronic.medical.record..EMR..data.for.research.or.quality.improvement.prior.to.this.datathon,
+teamsize = Number.of.people.on.your.datathon.team,
+workedTeam = Have.you.worked.with.the.members.of.your.datathon.team.before,
+role.clinical = Your.role.on.your.team...mark.all.that.apply...choice.Content..clinical..expert.,
+role.lead = Your.role.on.your.team...mark.all.that.apply...choice.Project.lead.,
+role.reviewer = Your.role.on.your.team...mark.all.that.apply...choice.Chart.reviewer.,
+role.datasci = Your.role.on.your.team...mark.all.that.apply...choice.Data.scientist.,
+role.stats = Your.role.on.your.team...mark.all.that.apply...choice.Statistical.analyst.,
+role.datawarehouse = Your.role.on.your.team...mark.all.that.apply...choice.Data.warehouse.expert.,
+role.datamgr = Your.role.on.your.team...mark.all.that.apply...choice.Data.manager.,
+role.learner = Your.role.on.your.team...mark.all.that.apply...choice.Learner.,
+role.other = Your.role.on.your.team...mark.all.that.apply...choice.Other..please.specify..,
+role.othertext = Other..specify.1,
+know.avail.pre = How.knowledgeable.were.you.about.data.availability.at.BCM.prior.to.the.datathon,
+know.avail.post = How.knowledgeable.were.you.about.data.availability.at.BCM.following.the.datathon,
+know.use.pre = How.knowledgeable.were.you.about.how.to.use.the.BCM.data.warehouse.prior.to.the.datathon,
+know.use.post = How.knowledgeable.were.you.about.how.to.use.the.BCM.data.warehouse.following.the.datathon,
+know.limit.pre = What.was.your.understanding.of.BCM.data.warehouse.limitations.prior.to.the.datathon,
+know.limit.post = What.was.your.understanding.of.BCM.data.warehouse.limitations.following.the.datathon,
+effortHrs = Estimate.how.many.person.hours.you..personally..spent.on.datathon.work..including.time.spent.with.BCM.information.technology..IT.,
+itpercent = What.percentage.of.the.time.spent.on.datathon.work.was.with.BCM.IT,
+hard.datapull = How.difficult.was.obtaining.information.from.the.BCM.Data.warehouse,
+hard.datawork = How.difficult.was.working.with.BCM.Data.warehouse.files,
+completed = I.completed.my.project.by.the.end.of.the.datathon,
+answered = I.successfully.answered.my.clinical.question,
+collab.outside = I.collaborated.with.individuals.outside.of.my.department,
+collab.new = I.collaborated.with.new.individuals.as.part.of.the.datathon,
+future.studies = I.plan.to.conduct.future.studies.using.data.from.the.BCM.warehouse,
+pub.abstract = Our.team.datathon.project.resulted.in.a.scientific.abstract.for.a.local.or.national.meeting,
+pub.paper = Our.team.datathon.project.resulted.in.a.publication,
+valuable = Participating.in.the.BCM.datathon.was.a.valuable.experience,
+future.datathon = I.would.participate.in.a.future.BCM.datathon,
+commenttext = What.worked.well.or.didn.t.work..or.general.comments,
+complete = Complete.
+) -> renamed
+
+renamed
 
 
 
-
-
-
-#### Plot
+#### Plots
 
 # qplot(analytic_data$los.days.n) +
 # scale_x_log10() ->
