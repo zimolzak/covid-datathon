c4a4c8da50bfa354530ef3a73974d1ef10a8ef17
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-12-17 11:00:52 -0600

    Formally export one more PNG for paper.

--- survey.R
+++ survey.R
@@ -495,3 +495,4 @@ ggsave(here('pngs-conf', 'amia-8-pair3.png'), paired3 + gfontsize(30))
 # paper
 ggsave(here('pngs-paper', 'facet.png'), facet)
 ggsave(here('pngs-paper', 'success_stack.png'), success_stack)
+ggsave(here('pngs-paper', 'roles.png'), barmaybe + labs(x="Role", title=NULL))
